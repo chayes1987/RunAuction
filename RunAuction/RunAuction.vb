28873516ab71cd59809755ecfbd69552265d54eb
Conor Hayes <ch1987@live.ie>
2015-01-30 18:19:47 +0000

    Added library for CouchDB using NuGet, make a connection to database from application

--- RunAuction/RunAuction.vb
+++ RunAuction/RunAuction.vb
@@ -1,4 +1,13 @@
-﻿Public Class RunAuction
+﻿Imports Dreamseat
 
+Public Class RunAuction
+    Private Const DB_NAME As String = "auctions"
+
+    Public Shared Sub Main()
+        Dim client As New CouchClient()
+        Dim db As CouchDatabase = client.GetDatabase(DB_NAME)
+        Console.WriteLine(db)
+        Console.ReadLine()
+    End Sub
 
 End Class
