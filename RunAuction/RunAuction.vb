ea95554cb78c2a4a7b2b6c99e0d79f3d6864ae32
Conor Hayes <ch1987@live.ie>
2015-01-30 19:17:20 +0000

    Added library for ZMQ. Subscribed to AuctionStarted event. Put it on separate thread because the main algorithm will run on the main thread.

--- RunAuction/RunAuction.vb
+++ RunAuction/RunAuction.vb
@@ -1,13 +1,34 @@
-﻿Imports Dreamseat
+﻿Imports DreamSeat
+Imports ZeroMQ
+Imports ZeroMQ.Sockets
+Imports System.Threading
 
-Public Class RunAuction
+
+Public Module RunAuction
     Private Const DB_NAME As String = "auctions"
+    Private context As IZmqContext = ZmqContext.Create()
+    Private auctionRunning As ISendSocket
 
-    Public Shared Sub Main()
+    Public Sub Main()
         Dim client As New CouchClient()
         Dim db As CouchDatabase = client.GetDatabase(DB_NAME)
-        Console.WriteLine(db)
-        Console.ReadLine()
+
+        Dim subscribeToAuctionStartedThread As New Thread(AddressOf SubscribeToAuctionStarted)
+        subscribeToAuctionStartedThread.Start()
     End Sub
 
-End Class
+    Private Sub SubscribeToAuctionStarted()
+        Dim subscriber As ISubscribeSocket = context.CreateSubscribeSocket()
+        subscriber.Connect("tcp://127.0.0.1:1001")
+        Dim prefix As Byte() = System.Text.Encoding.ASCII.GetBytes("AuctionStarted")
+        subscriber.Subscribe(prefix)
+        Console.WriteLine("Subscribed to AuctionStarted event...")
+
+        While (True)
+            Dim message As Byte() = subscriber.Receive()
+            Dim receiveStr As String = System.Text.Encoding.ASCII.GetString(message)
+            Console.WriteLine(receiveStr)
+        End While
+    End Sub
+
+End Module
